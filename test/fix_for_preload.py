f7ae4c9c769a22b0305a1593a9f0ba30cd200219
leilabbb <leila.ocean@gmail.com>
2018-04-20 10:37:50 -0400

    fix temperature units in ParameterDefs.csv  a preload-database file

--- test/fix_for_preload.py
+++ test/fix_for_preload.py
@@ -2,22 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import codecs
-import chardet
 import pandas as pd
 
 
 target_file = codecs.open('/Users/leila/Documents/OOI_GitHub_repo/work/002database/preload_ParameterDefs_data.csv', 'w','utf-8')
 
-url = 'https://raw.githubusercontent.com/oceanobservatories/preload-database/master/csv/ParameterDefs.csv'
-preload_data = pd.read_csv(url)
+preload_data = pd.read_csv('https://raw.githubusercontent.com/oceanobservatories/preload-database/master/csv/ParameterDefs.csv')
 
+unit_2_change = '/Users/leila/Documents/OOI_GitHub_repo/work/002database/test.csv'
 
-tunit = '/Users/leila/Documents/OOI_GitHub_repo/work/002database/test.csv'
+unit_preferred = 'ºC'
 
-
-tu = pd.read_csv(tunit)
+tu = pd.read_csv(unit_2_change)
 for i in range(len(tu['_Unit'])):
-    preload_data = preload_data.replace(tu['_Unit'][i],'ºC',regex=True)
+    preload_data = preload_data.replace(tu['_Unit'][i], unit_preferred,regex=True)
 
 
 preload_data.to_csv('/Users/leila/Documents/OOI_GitHub_repo/work/002database/preload_ParameterDefs_data.csv', index=False)
